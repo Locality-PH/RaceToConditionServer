bf1e15c2414b72ae7789f06fb0b02a4e156cfe6c
GiannPogi <gcmediavillo@gmail.com>
2021-10-09 22:06:05 +0800

    buy and sell funtion fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    port : 3306,
+    port: 3306,
     password: '',
     database: 'onlinemarket'
 });
@@ -17,73 +17,63 @@ const db = mysql.createConnection({
 app.get('/users/:id', (req, res) => {
 
     db.query('Select * FROM users WHERE id = ' + req.params.id,
-    (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.status(201).send(result);
-        }
-    });
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.status(201).send(result);
+            }
+        });
 });
 
 app.get('/users/:userId/cart', (req, res) => {
     const userId = req.params.userId;
 
-    db.query('SELECT c.id, i.name, i.price FROM items i INNER JOIN cart c ON i.id = c.item_id WHERE c.user_id = ? ORDER by id DESC',[userId],
-    (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.status(201).send(result);
-        }
-    });
+    db.query('SELECT c.id, c.item_id, i.name, i.price FROM items i INNER JOIN cart c ON i.id = c.item_id WHERE c.user_id = ? ORDER by id DESC', [userId],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.status(201).send(result);
+            }
+        });
 });
 
 app.get('/market/items', (req, res) => {
 
     db.query('Select * FROM items',
-    (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.status(201).send(result);
-        }
-    });
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.status(201).send(result);
+            }
+        });
 });
 
 app.patch('/market/:userId/buy/', (req, res) => {
     const userId = req.params.userId;
     const itemId = req.body.itemId;
-    const itemPrice = req.body.itemPrice;
-    let userMoney;
-    let totalMoney;
+    let itemPrice, userMoney, totalMoney;
 
     const setMoney = (money) => {
-    userMoney = money;
+        userMoney = money;
     }
 
-    db.query('Select * FROM users WHERE id = ' + userId,
-        (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                setMoney(result[0].money);
-                getTotalMoney(userMoney);
-                buyItem(userMoney);
-            }
-        });
-    
-    
-        const getTotalMoney = (money) => {
-           totalMoney = money - itemPrice;
-        }
+    const setItemPrice = (money) => {
+        itemPrice = money;
+    }
 
-        const buyItem = (currentMoney) => {
-            if (totalMoney < 0) {
-                res.status(201).send(false);
-            } else {
-                console.log("before update " + userMoney);
-                db.query("UPDATE `users` SET `money`= ? WHERE id = ?", [totalMoney, userId],
+    const setTotalMoney = (money) => {
+        totalMoney = money - itemPrice;
+    }
+
+    const buyItem = () => {
+        if (totalMoney < 0) {
+            res.status(201).send(false);
+        } else {
+            console.log("before update " + userMoney);
+            db.query("UPDATE `users` SET `money`= ? WHERE id = ?", [totalMoney, userId],
                 (err, result) => {
                     if (err) {
                         console.log(err)
@@ -91,8 +81,8 @@ app.patch('/market/:userId/buy/', (req, res) => {
                         console.log("Your money balance: " + totalMoney);
                     }
                 });
-            
-                db.query("INSERT INTO cart(user_id, item_id) VALUES (?,?)", [userId,itemId],
+
+            db.query("INSERT INTO cart(user_id, item_id) VALUES (?,?)", [userId, itemId],
                 (err, result) => {
                     if (err) {
                         console.log(err)
@@ -100,38 +90,90 @@ app.patch('/market/:userId/buy/', (req, res) => {
                         res.status(201).send("Post Sucess");
                     }
                 });
-            }
         }
-   
+    }
+
+    db.query('Select * FROM items WHERE id = ' + itemId,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                setItemPrice(result[0].price);
+            }
+        });
+
+    db.query('Select * FROM users WHERE id = ' + userId,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                setMoney(result[0].money);
+                setTotalMoney(userMoney);
+                buyItem();
+            }
+        });
+
+
 });
 
 app.patch('/market/:userId/sell/', (req, res) => {
     const userId = req.params.userId;
+    const cartId =req.body.cartId;
     const itemId = req.body.itemId;
-    const itemPrice = parseInt(req.body.itemPrice);
-    const userMoney = parseInt(req.body.userMoney);
-    var totalMoney = userMoney + itemPrice;
+    let itemPrice, userMoney, totalMoney;
 
-    console.log(totalMoney);
+    const setMoney = (money) => {
+        userMoney = money;
+    }
 
-    db.query("UPDATE `users` SET `money`= ? WHERE id = ?", [totalMoney, userId],
+    const setItemPrice = (money) => {
+        itemPrice = money;
+    }
+
+    const setTotalMoney = (money) => {
+        totalMoney = money + itemPrice;
+    }
+
+
+    const sellItem = () => {
+        db.query("UPDATE `users` SET `money`= ? WHERE id = ?", [totalMoney, userId],
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Your money balance: " + totalMoney);
+                }
+            });
+
+        db.query("DELETE FROM cart WHERE cart.id = ?", [cartId],
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.status(201).send("Delete Sucess");
+                }
+            });
+    }
+
+    db.query('Select * FROM items WHERE id = ' + itemId,
     (err, result) => {
         if (err) {
             console.log(err)
         } else {
-            console.log("Your money balance: " + totalMoney);
+            setItemPrice(result[0].price);
         }
     });
 
-    db.query("DELETE FROM cart WHERE cart.id = ?", [itemId],
-    (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.status(201).send("Delete Sucess");
-        }
-    });
-    
+    db.query('Select * FROM users WHERE id = ' + userId,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                setMoney(result[0].money);
+                setTotalMoney(userMoney);
+                sellItem();
+            }
+        });
 
 });
 
